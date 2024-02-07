43813d61b54c05a7d6b6778f7884e45873e4290b
pratik <pratikbhingradiya888@gmail.com>
2024-02-06 23:01:14 -0500

    changed header

--- front-end/src/components/header.jsx
+++ front-end/src/components/header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Navbar, Nav } from "react-bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css"; // Import Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const Header = () => {
   return (
