e684fb025304531ea3381f359bce74b2fbb6ab57
Krunal Priyadarshi <krunalpriyadarshi99@gmail.com>
2024-02-14 10:43:59 -0500

    Formatting of App.js
    Formatting of code structure

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,12 +1,13 @@
 import "./App.css";
 import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+{/* Component List */}
 import Header from "./components/header";
 import Home from "./components/home";
 import Footer from "./components/footer";
 import UserLogin from "./components/userLogin";
 import Logout from "./components/logout";
-import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import RegisterForm from "./components/register";
 import _404Page from "./components/404Page"
 
@@ -14,17 +15,15 @@ const App = () => {
   return (
     <div className="min-vh-100 d-flex flex-column">
       <Header />
-
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<UserLogin />} />
-          <Route path="/register" element={<RegisterForm />} />
-          <Route path="/logout" element={<Logout />} />
-          <Route path="*" element={<_404Page />} />
-        </Routes>
-      </Router>
-
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<UserLogin />} />
+            <Route path="/register" element={<RegisterForm />} />
+            <Route path="/logout" element={<Logout />} />
+            <Route path="*" element={<_404Page />} />
+          </Routes>
+        </Router>
       <Footer />
     </div>
   );
